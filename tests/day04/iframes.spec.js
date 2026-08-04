bf2258d7dce491d1796d1ccb3eb55a00a66a2dfa
AJan <wakarjan@hotmail.com>
2026-08-04 15:25:27 -0400

    iFrame

---
+++ tests/day04/iframes.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("", () => {
+
+    //create beforeEach method
+    
+    test.beforeEach(async( {page} )=>{
+      await page.goto('https://admin:admin@the-internet-5chk.onrender.com/iframe');
+      expect(await page.title() ).toBe("Practice");
+    })
+    
+    
+  test("@myFrame iframe test", async ({ page }) => {
+
+  let myFrame =  page.frameLocator("//iframe[@id='mce_0_ifr']");
+
+  let myFrameTextArea = myFrame.locator("//body[@id='tinymce']");  
+  
+  // await myFrameTextArea.clear(); 
+  //------OR-------
+  await myFrameTextArea.press("Control+A", "Backspace") //or "delete" // clearing with keyboard action
+
+  await myFrameTextArea.fill("Hello my Frame");
+
+  expect(myFrameTextArea).toHaveText("Hello my Frame")
+
+  console.log(myFrameTextArea.allInnerTexts());
+  });
+
+});
