da528484342d6d23987fbd0cc50637b38e0dac62
AJan <wakarjan@hotmail.com>
2026-08-04 20:03:51 -0400

    Mouse Actions

--- tests/day04/iframes.spec.js
+++ tests/day04/iframes.spec.js
@@ -4,10 +4,10 @@ test.describe("Test Group", () => {
 
     //create beforeEach method
     
-    test.beforeEach(async( {page} )=>{
-      await page.goto('https://admin:admin@the-internet-5chk.onrender.com/iframe');
-      expect(await page.title() ).toBe("Practice");
-    })
+  test.beforeEach(async( {page} )=>{
+    await page.goto('https://admin:admin@the-internet-5chk.onrender.com/iframe');
+    expect(await page.title() ).toBe("Practice");
+  })
     
     
   test("@myFrame -iframe test", async ({ page }) => {
@@ -15,10 +15,9 @@ test.describe("Test Group", () => {
     let myFrameTextArea = myFrame.locator("//body[@id='tinymce']");  
     // await myFrameTextArea.clear(); 
     //------OR-------
-    await myFrameTextArea.press("Control+A", "Backspace") //or "delete" // clearing with keyboard action
+    await myFrameTextArea.press("Control+A", "Backspace"); //or "delete" // clearing with keyboard action
     await myFrameTextArea.fill("Hello my Frame");
-    expect(myFrameTextArea).toHaveText("Hello my Frame")
-
+    expect(myFrameTextArea).toHaveText("Hello my Frame");
   });
 
 });
